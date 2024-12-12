f931d71dfb7562bb525fddd0b154abcffd9ba66c
butterdogg1467 <momink2600@gmail.com>
2024-12-12 17:54:16 -0500

    Fixed bug where styles and js was not being loaded correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
   },
   module: {
     rules: [
@@ -15,12 +13,20 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
-    plugins: [
+  plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'page.html',
-    })
-    ]
+      template: './src/index.html',
+    }),
+  ],
+  mode: 'development',
+  devServer: {
+    contentBase: './dist',
+    open: true,
+  },
 };
